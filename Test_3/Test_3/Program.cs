f70f6d1cbe6c665afa1c41f4559c85d21ea217cb
Vanyul <164251530+Vanyul@users.noreply.github.com>
2025-07-29 16:07:45 +0300

    init

---
+++ Test_3/Test_3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class BaseConverter
+{
+    private double celsius;
+
+    public BaseConverter(double celsius)
+    {
+        this.celsius = celsius;
+    }
+
+    public double Convert(string toUnit)
+    {
+        return toUnit.ToLower() switch
+        {
+            "k" => celsius + 273.15,             
+            "f" => celsius * 9 / 5 + 32,     
+            _ => throw new ArgumentException("Неизвестная единица")
+        };
+    }
+}
+
+class Task3
+{
+    public static void Main()
+    {
+        Console.Write("Введите температуру в Цельсиях: ");
+        double temp = double.Parse(Console.ReadLine());
+
+        Console.Write("Введите 'k' для Кельвинов или 'f' для Фаренгейтов: ");
+        string unit = Console.ReadLine();
+
+        BaseConverter converter = new BaseConverter(temp);
+        double result = converter.Convert(unit);
+        Console.WriteLine($"Результат: {result:F2}");
+    }
+}
